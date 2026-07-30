0e033bdd98393868f63b5539946af86fb641f963
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-07-29 22:39:04 -0500

    feat: replace OrganizationsPageView with OrganizationsPage component in OrganizationsRoutePage

--- app/(protected)/(configuration)/organizations/page.tsx
+++ app/(protected)/(configuration)/organizations/page.tsx
@@ -1,9 +1,9 @@
 import { createOrganizationQueryService } from "@/contexts/business/application/internal/queryservices/organization-query.service";
-import { OrganizationsPageView } from "@/contexts/business/interfaces/components/organization/organizations-page/organizations-page-view";
+import { OrganizationsPage } from "@/contexts/business/interfaces/components/organization/organizations-page/organizations-page";
 
 export default async function OrganizationsRoutePage() {
   const organization =
     await createOrganizationQueryService().getMyOrganization();
 
-  return <OrganizationsPageView organization={organization} />;
+  return <OrganizationsPage organization={organization} />;
 }
